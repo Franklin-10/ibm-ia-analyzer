3340246dc8b03ea8d8457ea399159b40d68b80e4
Franklin-10 <franklin.oliveira096@gmail.com>
2026-05-21 19:27:46 -0400

    Adicionado tarefas 2,3 e 4

---
+++ 3a_outpout_formatting.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def emotion_detecter(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers = headers)
+    formated = json.loads(response.text)
+    emotions = formated['emotionPredictions'][0]['emotion']
+    new_value = 0
+    dominant_emotion = ''
+    for emotion, value in emotions.items():
+        if new_value < value:
+            new_value = value
+            dominant_emotion = emotion
+    
+    emotions['dominant_emotion'] = dominant_emotion
+    return emotions
+
+emotion_detecter('I love Cheese')
+
