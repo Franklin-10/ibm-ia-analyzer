dddc717a1ad088d36ff957f55568f573ed6a88da
= <=>
2026-05-21 22:03:02 -0400

    Enviado tasks 5 e parcial 6

--- server.py
+++ server.py
@@ -2,27 +2,29 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask
+from EmotionDetection.emotion_detection import emotion_detecter
 
-#Initiate the flask app : TODO
+app = Flask('Emotion Detector')
 
-@app.route("/sentimentAnalyzer")
-def sent_analyzer():
+@app.route("/emotionDetector")
+def sent_emotion():
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
-
+    text_to_analyze = requests.args.get('textToAnalyze')
+    response = emotion_detecter(text_to_analyze)
+    emotion = response['']
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template('index.html')
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
+    app.run(host="0.0.0.0", port=5000)
---
+++ test_emotion_detection.py
@@ -0,0 +1,22 @@
+from EmotionDetection.emotion_detection import emotion_detecter
+import unittest
+
+class TestEmotionDetection(unittest.TestCase):
+    def test_emotion(self):
+        #Test Case for joy emotion
+        result_1 = emotion_detecter('I am glad this happened')
+        self.assertEqual(result_1['dominant_emotion'], 'joy')
+        #Test Case for anger emotion
+        result_2 = emotion_detecter('I am really mad about this')
+        self.assertEqual(result_2['dominant_emotion'], 'anger')
+        #Test Case for disgust emotion
+        result_3 = emotion_detecter('I feel disgusted just hearing about this')
+        self.assertEqual(result_3['dominant_emotion'], 'disgust')
+        #Test Case for sadness emotion
+        result_4 = emotion_detecter('I am so sad about this')
+        self.assertEqual(result_4['dominant_emotion'], 'sadness')
+        #Test Case for fear emotion
+        result_5 = emotion_detecter('I am really afraid that this will happen')
+        self.assertEqual(result_5['dominant_emotion'], 'fear')
+
+unittest.main()
