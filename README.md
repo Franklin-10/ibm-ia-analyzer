42d984a74cf5926f5e5a6f3469e68762d22d94d1
= <=>
2026-05-25 21:45:33 -0400

    Ajustes nos arquivos test adicionado caso None e readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Final project
 
+Final Project
+
 Este é um projeto prático de um analisador de sentimetos utilizando a IA Watson com a biblioteca NLP da IBM.
--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,22 +1,26 @@
-from EmotionDetection.emotion_detection import emotion_detecter
+from EmotionDetection.emotion_detection import emotion_detector
 import unittest
 
 class TestEmotionDetection(unittest.TestCase):
     def test_emotion(self):
         #Test Case for joy emotion
-        result_1 = emotion_detecter('I am glad this happened')
+        result_1 = emotion_detector('I am glad this happened')
         self.assertEqual(result_1['dominant_emotion'], 'joy')
         #Test Case for anger emotion
-        result_2 = emotion_detecter('I am really mad about this')
+        result_2 = emotion_detector('I am really mad about this')
         self.assertEqual(result_2['dominant_emotion'], 'anger')
         #Test Case for disgust emotion
-        result_3 = emotion_detecter('I feel disgusted just hearing about this')
+        result_3 = emotion_detector('I feel disgusted just hearing about this')
         self.assertEqual(result_3['dominant_emotion'], 'disgust')
         #Test Case for sadness emotion
-        result_4 = emotion_detecter('I am so sad about this')
+        result_4 = emotion_detector('I am so sad about this')
         self.assertEqual(result_4['dominant_emotion'], 'sadness')
         #Test Case for fear emotion
-        result_5 = emotion_detecter('I am really afraid that this will happen')
+        result_5 = emotion_detector('I am really afraid that this will happen')
         self.assertEqual(result_5['dominant_emotion'], 'fear')
+    
+    def test_invalid_emotion(self):
+        result = emotion_detector('')
+        self.assertIsNone(result)
 
 unittest.main()
